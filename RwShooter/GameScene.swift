831fb99f87d13c8ed246eb5c1e901f074a631535
meamuri <meammf@yandex.ru>
2020-11-08 21:19:16 +0300

    implement some steps of tutorial

--- RwShooter/GameScene.swift
+++ RwShooter/GameScene.swift
@@ -10,79 +10,55 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var player: SKNode?
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let playerNode = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 45, height: 45), cornerRadius: 10.0)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        playerNode.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        playerNode.fillColor = .black
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        backgroundColor = .white
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        player = playerNode
+        addChild(player!)
+        
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(addMonster),
+                SKAction.wait(forDuration: 1.0)
+            ])
+        ))
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    private func addMonster() {
+        let monster = SKShapeNode(rect: CGRect(x: 0, y: 0, width: self.monsterSpriteWidth, height: monsterSpriteHeight), cornerRadius: 10.0)
+        let actualY = random(min: monsterSpriteHeight / 2.0, max: size.height - monsterSpriteHeight / 2.0)
+        
+        monster.position = CGPoint(x: size.width + monsterSpriteWidth / 2, y: actualY)
+        monster.fillColor = .brown
+        
+        addChild(monster)
+        
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        let actionMove = SKAction.move(to: CGPoint(x: -monsterSpriteWidth/2.0, y: actualY), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        monster.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    private func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / Float(0xFFFFFFFF))
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    private func random(min: CGFloat, max: CGFloat) -> CGFloat {
+      return random() * (max - min) + min
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+    private let monsterSpriteHeight = CGFloat(50)
+    private let monsterSpriteWidth = CGFloat(50)
 }
